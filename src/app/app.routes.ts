b0866206a3eb9013d97918f3258af9d33622fa98
yasmine <yasmine.khamlichi@frequentis.com>
2026-01-14 11:59:02 +0100

    setup with token

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+//import { HomeComponent } from './home/home.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CallbackComponent } from './callback/callback.component';
+import { authGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'callback', component: CallbackComponent },
+  { path: 'employees', component: EmployeeListComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' }
+];
