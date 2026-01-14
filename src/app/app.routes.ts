24f7688a047eb22c0aae19a18e6bccc99fb8af67
Rebecca <rebecca.r3@schule.bremen.de>
2026-01-14 11:27:49 +0100

    LF10-71 setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+// @ts-ignore
+import { HomeComponent } from './home/home.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CallbackComponent } from './callback/callback.component';
+import { authGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'callback', component: CallbackComponent },
+  { path: 'employees', component: EmployeeListComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: '' }
+];
