1699ce2dbd11bb65320365e922dfeac0ddaeee33
Rebecca <rebecca.r3@schule.bremen.de>
2026-01-28 11:13:39 +0100

    WIP

--- src/app/QualfikationService.ts
+++ src/app/QualfikationService.ts
@@ -4,6 +4,8 @@ import {AuthService} from "./auth.service";
 import {Observable} from "rxjs";
 import {Qualification} from "./Qualification";
 
+import {TokenService} from './TokenService';
+
 @Injectable({
     providedIn: 'root'
 })
@@ -11,11 +13,11 @@ export class QualificationService {
     private apiUrl = 'http://localhost:8089/qualifications';
   //private apiUrl = 'https://employee.szut.dev/qualifications'
 
-    constructor(private http: HttpClient, private authService: AuthService) {}
+    constructor(private http: HttpClient, private authService: AuthService, private tokenService: TokenService) {}
 
     private getHeaders(): HttpHeaders {
         //const token = this.authService.getAccessToken();
-        const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjM0YzgyYmEwZDU1NWM5NDkzODg5Y2ViMGNiZGI2NTI5IiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwMDAvYXBwbGljYXRpb24vby9lbXBsb3llZV9hcGkvIiwic3ViIjoiYTA0NjM3NzlkYWNhMDY4ZTAzNWFiMTg3NzI1OGYzOTk5MjljNTA3ODAyYTQ2YjVlMWZkZjQ2NDU1ZmY5ZGYxYiIsImF1ZCI6ImVtcGxveWVlX2FwaV9jbGllbnQiLCJleHAiOjE3Njk1OTUwNzksImlhdCI6MTc2OTU5MjA3OSwiYXV0aF90aW1lIjoxNzY5NTkyMDc5LCJhY3IiOiJnb2F1dGhlbnRpay5pby9wcm92aWRlcnMvb2F1dGgyL2RlZmF1bHQiLCJhenAiOiJlbXBsb3llZV9hcGlfY2xpZW50IiwidWlkIjoid2FnT3BhU0VBaXpzM0liUExNeDNsTnhjYTUycWJEYkcyRTJxeTl6VCJ9.eeTT62i1IB6BxJrY5giSNH_PgE-SbpXDwujzoIcuJjlohCeoXFZIhOj3qglzIhDLniTtzK8GvltE0cgoIWX5aoFTZicetPrBOcIyinPxXOe3ixHeBlYtSHKITrNzH0qltw8ekxjOLjKqpITOEygDmNLfIp9MYuetBuPhiMjMi9U9ltGXGIsCtb2x59Pz_SvrrnK09j1OZRdxTgOC8kAFt9UEYWLVd0EFF3784JaKJ29HpF1Fp9Pt-qKDhYa4ZIvrUTKdjF_RR9pitgEGARYMFqIIRolfwlW_Q59NSP4A92v_rdo4oJA3h2Ptngb7yP9LmQA5JnKPiGnkJWYhkm0NJP--kAlSeIVMWtCxE7wc8tPiAlvDpJERghgI1sR1sDe1f_a9omQGzcW9vJ614kBxHtBZqPz2sQKMYeu8Oq0EttZoVhwzt3nSUqArHuC77d_GO2WyJuxC5UlB0bcVJyMDH0nZjjgKBH1amih6hosGECBG3i9obM4HCp6D5bJSNJIEvRj6bqey-qbQZGWPh6f557VVHOTz3rpYIz69SI-vPT5NN6esy5M1BPxklTIyqz0z6lLpdw2PbdbpWNiKCjctj66-y5H8f_BgMtHlLgJXJ3H1iiUt6JN63vP2yD3i7FkRJYMmI5BYiVluE2T8d6SJENnBlfAXNNn7ISGKbaIoEJ4'
+        const token =this.tokenService.getToken();
 
         return new HttpHeaders()
             .set('Authorization', `Bearer ${token}`)
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -9,6 +9,8 @@ import {EmployeeEditComponent} from "../employee-edit/employee-edit.component";
 import {EmployeeCreateComponent} from "../employee-create/employee-create.component";
 import {EmployeeDeleteComponent} from "../employee-delete/employee-delete.component";
 
+import {TokenService} from "../TokenService";
+
 @Component({
   selector: 'app-employee-list',
   standalone: true,
@@ -22,19 +24,19 @@ export class EmployeeListComponent {
 
   constructor(
     private http: HttpClient,
-    private authService: AuthService
+    private authService: AuthService,
+    private tokenService: TokenService
   ) {
     this.fetchData();
   }
 
   fetchData() {
-    const token ='eyJhbGciOiJSUzI1NiIsImtpZCI6IjM0YzgyYmEwZDU1NWM5NDkzODg5Y2ViMGNiZGI2NTI5IiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwMDAvYXBwbGljYXRpb24vby9lbXBsb3llZV9hcGkvIiwic3ViIjoiYTA0NjM3NzlkYWNhMDY4ZTAzNWFiMTg3NzI1OGYzOTk5MjljNTA3ODAyYTQ2YjVlMWZkZjQ2NDU1ZmY5ZGYxYiIsImF1ZCI6ImVtcGxveWVlX2FwaV9jbGllbnQiLCJleHAiOjE3Njk1OTUwNzksImlhdCI6MTc2OTU5MjA3OSwiYXV0aF90aW1lIjoxNzY5NTkyMDc5LCJhY3IiOiJnb2F1dGhlbnRpay5pby9wcm92aWRlcnMvb2F1dGgyL2RlZmF1bHQiLCJhenAiOiJlbXBsb3llZV9hcGlfY2xpZW50IiwidWlkIjoid2FnT3BhU0VBaXpzM0liUExNeDNsTnhjYTUycWJEYkcyRTJxeTl6VCJ9.eeTT62i1IB6BxJrY5giSNH_PgE-SbpXDwujzoIcuJjlohCeoXFZIhOj3qglzIhDLniTtzK8GvltE0cgoIWX5aoFTZicetPrBOcIyinPxXOe3ixHeBlYtSHKITrNzH0qltw8ekxjOLjKqpITOEygDmNLfIp9MYuetBuPhiMjMi9U9ltGXGIsCtb2x59Pz_SvrrnK09j1OZRdxTgOC8kAFt9UEYWLVd0EFF3784JaKJ29HpF1Fp9Pt-qKDhYa4ZIvrUTKdjF_RR9pitgEGARYMFqIIRolfwlW_Q59NSP4A92v_rdo4oJA3h2Ptngb7yP9LmQA5JnKPiGnkJWYhkm0NJP--kAlSeIVMWtCxE7wc8tPiAlvDpJERghgI1sR1sDe1f_a9omQGzcW9vJ614kBxHtBZqPz2sQKMYeu8Oq0EttZoVhwzt3nSUqArHuC77d_GO2WyJuxC5UlB0bcVJyMDH0nZjjgKBH1amih6hosGECBG3i9obM4HCp6D5bJSNJIEvRj6bqey-qbQZGWPh6f557VVHOTz3rpYIz69SI-vPT5NN6esy5M1BPxklTIyqz0z6lLpdw2PbdbpWNiKCjctj66-y5H8f_BgMtHlLgJXJ3H1iiUt6JN63vP2yD3i7FkRJYMmI5BYiVluE2T8d6SJENnBlfAXNNn7ISGKbaIoEJ4'
+    const token =this.tokenService.getToken();
     this.http.get<Employee[]>('http://localhost:8089/employees', {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
     }).subscribe({
-      next: list => this.employeesSubject.next(list),
       next: list => this.employeesSubject.next(list),
       error: err => console.error(err)
     });
