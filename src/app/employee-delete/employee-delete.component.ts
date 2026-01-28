bcecd15da347be26ce0e92cc18696f35cb1989c3
ave <ave@acadon.de>
2026-01-28 10:29:01 +0100

    new

--- src/app/employee-delete/employee-delete.component.ts
+++ src/app/employee-delete/employee-delete.component.ts
@@ -1,16 +1,16 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Employee } from '../Employee';
+import {FormsModule} from "@angular/forms";
 
 @Component({
-  selector: 'app-employee-edit',
+  selector: 'app-employee-delete',
   standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './employee-edit.component.html',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './employee-delete.component.html'
 })
-export class EmployeeEditComponent {
+export class EmployeeDeleteComponent {
   @Output() saved = new EventEmitter<void>();
 
   show = false;
@@ -27,35 +27,25 @@ export class EmployeeEditComponent {
     this.show = false;
   }
 
-  save() {
-    if (!this.employee) return;
+  confirmDelete() {
+    if (!this.employee?.id) return;
 
     const token ='eyJhbGciOiJSUzI1NiIsImtpZCI6IjM4ODFhYmEzMTRjYTdhYmM0MWFjOTZiNWJlZThmMzNkIiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwMDAvYXBwbGljYXRpb24vby9lbXBsb3llZV9hcGkvIiwic3ViIjoiZjhjMDNjOWU5ZDliZDc0ZGY3ZDQwZjVjNjRlODI1NDNhYjY1YTIyMzQ3MTA5NTJhNzQwYzUxN2I5YTE0ZjU4MSIsImF1ZCI6ImVtcGxveWVlX2FwaV9jbGllbnQiLCJleHAiOjE3Njk1OTQxNDQsImlhdCI6MTc2OTU5MTE0NCwiYXV0aF90aW1lIjoxNzY5NTkxMTQ0LCJhY3IiOiJnb2F1dGhlbnRpay5pby9wcm92aWRlcnMvb2F1dGgyL2RlZmF1bHQiLCJhenAiOiJlbXBsb3llZV9hcGlfY2xpZW50IiwidWlkIjoiSWVwQXo5M0s0RVNDZksyMTVVWGMxblZ5elRRQVFmUVJVbVlaOVJwYSJ9.ebzNR51yUpCRtR75hFXXwsXX6l1ipZbRh_OuM7wzJaK1PsgRSNCXoFod2JMBu46lZea1uRyZNB3yQwKy3hplVlmJGsdl12OKjK-uLlT7tQ0bHlxyfZ5QhrKr8Wm5uNNlHPK-T-tyhVlK9r2tWLJPJ7cfuZzQyMexlxx4v5eLX0TCp-fDw2GdaIhjJ6Ga8pEbD6ByjshaSMDju-TLcrYgeh14ebQcmqD7HqwsC58t5AyFql56mAg6YErS-K7t6IFpzEY1y9LJr76ToBI5ntjdH35jWH8x1FsjThEYfw7oKPo3jk3pViwl_QdKM3wvkEeAcM_i-heIJp8nzoIJJhKjIY07sWmpWCCrm2SMlDxw2NzVPBjPvr2tniL2f7hDrzatlL2hrWcKTYQDTHtXm71Dqf-CgyW9BTp35ohADqkyIvrhAedP_34d9yqZVgissLZp17BhLY7mZvEzgsCMU-HkryC-iHGmLU409ORGY-nST-7MKB3vWcyovtzvbPoCJVxLc41z4lN7ZVK3UNTKgUeC8hmUp-s7gKmKKO4H3OfBV_PUktHzwOtYwkad-2iKN3GVZojFeQc53gnif0lgd5HVv3caaDU3wo-7P-wgefeaGQZ7u6oC2oLWg0e6HBx5Tqi2C_GLKuomH_3jDqBFcMP-KpoBS1raG1fD82CeLfTOgLM'
 
-    const payload = {
-      id: this.employee.id,
-      firstName: this.employee.firstName,
-      lastName: this.employee.lastName,
-      street: this.employee.street,
-      postcode: this.employee.postcode,
-      city: this.employee.city,
-      phone: this.employee.phone
-    };
-    this.http.put(
+    this.http.delete(
       `http://localhost:8089/employees/${this.employee.id}`,
-      payload,
       {
         headers: new HttpHeaders()
-          .set('Content-Type', 'application/json')
           .set('Authorization', `Bearer ${token}`)
       }
     ).subscribe({
       next: () => {
-        console.log('Employee saved');
         this.saved.emit();
         this.close();
       },
-      error: err => console.error('Save failed', err)
+      error: err => console.error('Delete failed', err)
     });
   }
+
+
 }
