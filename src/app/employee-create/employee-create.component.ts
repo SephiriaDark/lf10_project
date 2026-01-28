67b64370681a3dad2e2e3b17861eaaea81334dec
ave <ave@acadon.de>
2026-01-28 13:01:47 +0100

    bugfix

--- src/app/employee-create/employee-create.component.ts
+++ src/app/employee-create/employee-create.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Employee } from '../Employee';
+import {TokenService} from "../TokenService";
 
 @Component({
   selector: 'app-employee-create',
@@ -23,15 +24,24 @@ export class EmployeeCreateComponent {
   };
 
   open() {
+    this.employee = {
+      firstName: '',
+      lastName: '',
+      phone: '',
+      street: '',
+      postcode: '',
+      city: '',
+      skillSet: []
+    };
     this.show = true;
   }
   close() {
     this.show = false;
   }
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   save() {
-    const token ='eyJhbGciOiJSUzI1NiIsImtpZCI6IjM0YzgyYmEwZDU1NWM5NDkzODg5Y2ViMGNiZGI2NTI5IiwidHlwIjoiSldUIn0.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwMDAvYXBwbGljYXRpb24vby9lbXBsb3llZV9hcGkvIiwic3ViIjoiYTA0NjM3NzlkYWNhMDY4ZTAzNWFiMTg3NzI1OGYzOTk5MjljNTA3ODAyYTQ2YjVlMWZkZjQ2NDU1ZmY5ZGYxYiIsImF1ZCI6ImVtcGxveWVlX2FwaV9jbGllbnQiLCJleHAiOjE3Njk1OTY2NjgsImlhdCI6MTc2OTU5MzY2OCwiYXV0aF90aW1lIjoxNzY5NTkzNjY4LCJhY3IiOiJnb2F1dGhlbnRpay5pby9wcm92aWRlcnMvb2F1dGgyL2RlZmF1bHQiLCJhenAiOiJlbXBsb3llZV9hcGlfY2xpZW50IiwidWlkIjoieDBoRmhRdEhhVUVMNW1JdFc3bHJxWVhSaFd2b2pxUUdOeFBnQlh1WiJ9.Pj1tG0v1CAHQFW_mBzOOGvmvdIV3PFVYCopQz-23IOHxayVtwX2MJzqiMmzH1M4vpt2akpqNorwnbzv1oAtLPTAnQGAMXKxiHzrl59A7Y5BLs8tKVYtOupKV0hoiHfSuv8skcjr29LdmyYhyYz0mrOcdbbKUzvgwL7ymwny0Oe-XQIwc9lw6eNymEnGCv0NFm0jqzr7o13CiU3xyvpV0dzhE64RJPtya8lR-YOcjPUHgvr7f1oCFY5GqOdTH-sb_a3VWLVF0pVwFWG_ppeKZro4UaQtrtW8mSPRKhdgDGG5JLrQmiOGVvnVxNtJBqo8Q_JROh405tNJk4_HoakNimhP_mVIc1iNWyXaOlGIE-j9ojMQPLQ7rhokiSgSmue4IReT5YNXLC6kcVZQ8FtkpvAXvOdOz1EKNMPlvH-lP3WRDQvVxt_SujBQHduNwPjCuaw_Is6bfF256RAjCEczkB4Fqq4Qc3953Iwx-2g1pB2nslTISBYAkMxWzQfBbvdOLcP6AXKd4qMJYA5jJK8p97Ri2T9_WyjaXlxVP1CShN90xbYjFeoZ9_xTzjRr1ER7g3oVD3iw1uiGC_3hnfkUErirDuBJIeuor36B35kETgvvUth2K6tao9vApFywHFPtrKwZUnhi0kh9tA65lbTYLm5POIWxeaU0PJCe4e9QACB0'
+    const token =this.tokenService.getToken();
 
     this.http.post(
       'http://localhost:8089/employees',
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -8,13 +8,14 @@ import {EmployeeDetailsComponent} from "../employee-details/employee-details.com
 import {EmployeeEditComponent} from "../employee-edit/employee-edit.component";
 import {EmployeeCreateComponent} from "../employee-create/employee-create.component";
 import {EmployeeDeleteComponent} from "../employee-delete/employee-delete.component";
+import {FormsModule} from "@angular/forms";
 
 import {TokenService} from "../TokenService";
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [CommonModule, EmployeeDetailsComponent, EmployeeEditComponent, EmployeeCreateComponent, EmployeeDeleteComponent],
+  imports: [CommonModule, EmployeeDetailsComponent, EmployeeEditComponent, EmployeeCreateComponent, EmployeeDeleteComponent, FormsModule],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
@@ -29,6 +30,14 @@ export class EmployeeListComponent {
   ) {
     this.fetchData();
   }
+  filter = {
+    firstName: '',
+    lastName: '',
+    city: '',
+    skill: ''
+  };
+
+  private allEmployees: Employee[] = [];
 
   fetchData() {
     const token =this.tokenService.getToken();
@@ -37,11 +46,47 @@ export class EmployeeListComponent {
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
     }).subscribe({
-      next: list => this.employeesSubject.next(list),
+      next: list => {
+        this.allEmployees = list;
+        this.employeesSubject.next(this.sortById(list))
+      },
       error: err => console.error(err)
     });
   }
   refreshList() {
     this.fetchData();
+  }
+  applyFilter() {
+    const filtered = this.allEmployees.filter(e => {
+
+      if (this.filter.firstName &&
+        !e.firstName?.toLowerCase().startsWith(this.filter.firstName.trim().toLowerCase())) {
+        return false;
+      }
+
+      if (this.filter.lastName &&
+        !e.lastName?.toLowerCase().startsWith(this.filter.lastName.trim().toLowerCase())) {
+        return false;
+      }
+
+      if (this.filter.city &&
+        !e.city?.toLowerCase().startsWith(this.filter.city.trim().toLowerCase())) {
+        return false;
+      }
+
+      if (this.filter.skill) {
+        const hasSkill = e.skillSet?.some(s =>
+          s.skill.toLowerCase().startsWith(this.filter.skill.trim().toLowerCase())
+        );
+        if (!hasSkill) return false;
+      }
+
+      return true;
+    });
+
+    this.employeesSubject.next(this.sortById(filtered));
+  }
+  private sortById(list: Employee[]): Employee[] {
+    return [...list].sort((a, b) => (a.id ?? 0) - (b.id ?? 0));
   }
 }
