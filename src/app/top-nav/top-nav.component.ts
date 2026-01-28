28c374885135d19fc01bfb3200c73a36335a295a
Rebecca <rebecca.r3@schule.bremen.de>
2026-01-28 08:20:45 +0100

    conflict

--- src/app/top-nav/top-nav.component.ts
+++ src/app/top-nav/top-nav.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-top-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.css'
 })
